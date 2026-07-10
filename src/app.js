6f0ccc311da637eca06ebbf0a513dc8a3ee48b6b
Girish <sonawanegirish630@email.com>
2026-07-10 20:49:21 +0530

    add utils for  error and response

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+
+const app=express()
+
+//.use is only used for middlewares and config
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 
 dotenv.config()
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is Running at PORT : ${process.env.PORT}`);
+    })
+    app.on("error",(error)=>{           //not necessary but to improve code base
+        console.log("ERROR:",error);
+        throw error;        
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed  !!!",err);
+    
+})
 
